f955674a80d170be413524208e03ed8a19c8d117
uhfug <kirill_ternivskii@mail.ru>
2022-01-13 11:50:22 +0500

    lesson5.2

--- lessson5.py
+++ lessson5.py
@@ -25,24 +25,32 @@ print(list(odd_to_n))
 '''
 #exercise 3
 
-from itertools import zip_longest
-
 tutors = [
         'Иван', 'Анастасия', 'Петр', 'Сергей',
         'Дмитрий', 'Борис', 'Елена'
     ]
 klasses = [
-        '9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А'
+        '9А', '7В', '9Б', '9В', '8Б', '10А',
     ]
-tutors_and_klasses =  zip_longest(tutors, klasses)
-for i in tutors_and_klasses:
-    print(i)
 
+tutors_and_klasses =(
+    (tutors[i], None) if i >= len(klasses)
+    else (tutors[i], klasses[i])
+    for i in range(len(tutors))
+)
 
-'''
-tutors_and_klasses =[(tutors, klasses) for tutors, klasses in zip(tutors, klasses)]
-'''
+print(type(tutors_and_klasses), *tutors_and_klasses)
 
-print(tutors_and_klasses)
+#exercise 4
 
-print((list(tutors_and_klasses)))
+src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+result_num = []
+
+result = (
+    result_num.append(src[i]) if i != 0 and src[i] > src[i - 1]
+    else ()
+    for i in range(len(src))
+)
+
+(list(result))
+print(result_num)
